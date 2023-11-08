3a578bf4b61cc47f103222cf915f1c177bc70608
Sirabax <xabaris@gmail.com>
2023-11-08 09:52:03 +0100

    testimonial update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,28 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\CommentsRepository; // Import the CommentsRepository
 
 class HomeController extends AbstractController
 {
-    private $entityManager;
-    private $commentsRepository;
-
-    public function __construct(EntityManagerInterface $entityManager, CommentsRepository $commentsRepository)
-    {
-        $this->entityManager = $entityManager;
-        $this->commentsRepository = $commentsRepository;
-    }
-
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        // Fetch comments from the database
-        $comments = $this->commentsRepository->findAll();
-
-        return $this->render('comments.html.twig', [
-            'comments' => $comments,
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
