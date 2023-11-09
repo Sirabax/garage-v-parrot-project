75ce4ab2c6ea4bddd5cf01c1382071166efca73d
TittoPaolo <nelo-angelo@hotmail.it>
2023-11-09 11:47:18 +0100

    monolog

--- config/update-moderation.php
+++ config/update-moderation.php
@@ -1,4 +1,8 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Connect to your MySQL database using MySQLi
 $servername = "localhost"; // Replace with your MySQL server address
 $username = "root"; // Replace with your MySQL username
@@ -20,13 +24,29 @@ $isChecked = $_POST['isChecked'];
 // Prepare and execute the SQL statement to update the moderation column in the 'comments' table
 $sql = "UPDATE comments SET moderation = ? WHERE id = ?";
 $stmt = $conn->prepare($sql);
+
+// Check if the SQL statement preparation was successful
+if (!$stmt) {
+    die("Error preparing statement: " . $conn->error);
+}
+
+// Bind parameters
 $stmt->bind_param("ii", $isChecked, $itemId);
 
+// Check if binding parameters was successful
+if (!$stmt) {
+    die("Error binding parameters: " . $conn->error);
+}
+
+// Execute the prepared statement
 if ($stmt->execute()) {
     echo "Moderation status updated successfully.";
 } else {
     echo "Error updating moderation status: " . $stmt->error;
 }
+
+// Close the statement
+$stmt->close();
 
 // Close the connection
 $conn->close();
